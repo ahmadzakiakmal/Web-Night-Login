dbf2a3eaa91c53171bdb71102f50389cdf9273de
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-08 21:23:19 +0700

    fix: properly aligned Hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,11 +1,13 @@
-import { HeroLines } from "./HeroLines"
-import { HeroMain } from "./HeroMain"
+import { HeroMain } from "../HeroMain";
+import { HeroLines } from "./HeroLines";
 
 export const Hero = () => {
-  return(
-    <div className="bg-white h-[92vh] w-full relative">
+  return (
+    <div className="bg-white h-[92vh] w-full relative flex justify-center items-center">
+      {/* png / svg  */}
       <HeroLines />
+      {/* logo + text */}
       <HeroMain />
     </div>
-  )
-}
+  );
+};
