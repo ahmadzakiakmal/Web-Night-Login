0590501cc45284b29d058b350ff49f5d247b615f
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-09 14:46:51 +0700

    fix: change elements alignment

--- components/AboutUs/AboutUs.tsx
+++ components/AboutUs/AboutUs.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { AboutUsLines } from "./AboutUsLines";
 
 export const AboutUs = () => {
@@ -7,17 +8,26 @@ export const AboutUs = () => {
       <div className="w-[20%] h-[100vh] bg-dark-red absolute top-0 right-0 z-0"></div>
       <AboutUsLines />
       {/* content */}
-      <div className="z-10 w-3/4">
-        <span className="text-6xl text-[#494949] font-bold">About Us</span>
-        <div className="flex max-w-[50ch] text-[24px]">
-          <p>
+      <div className="z-10 w-[90%]">
+        <div className="flex text-[24px] justify-center">
+          <p className="max-w-[50ch] pr-5 self-center">
+            <h1 className="text-6xl text-[#494949] font-bold mb-[20px] w-fit">
+              About Us
+            </h1>
             Night Login merupakan Badan Semi Otonom (BSO) di DTETI FT UGM yang
             bergerak di bidang teknologi informasi. BSO ini merupakan wadah bagi
             mahasiswa DTETI yang memiliki minat di bidang IT untuk mengembangkan
             kemampuan, kreativitas, inovasi, dan memperluas wawasan terkait
             bidang-bidang IT.
           </p>
-          <div></div>
+          <div className="h-[300px] aspect-square">
+            <Image
+              src="/NL Assets/Night Login/square.png"
+              width={2000}
+              height={1000}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
