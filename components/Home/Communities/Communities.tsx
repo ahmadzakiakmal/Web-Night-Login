a659517d95fb550467b9275cbdc0c24da1a687bd
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-12 11:02:32 +0700

    feat: update Communities section

--- components/Home/Communities/Communities.tsx
+++ components/Home/Communities/Communities.tsx
@@ -1,21 +1,20 @@
-import { CommunitiesLines } from "./CommunitiesLines"
-import { CommunityCard } from "./CommunityCard"
+import { CommunitiesLines } from "./CommunitiesLines";
+import { CommunityCard } from "./CommunityCard";
 
 export const Communities = () => {
-  const communities = ["NLNC", "NUXC", "NCPC", "NADC", "NWDC", "NDDC"]
-  return(
-    <div className="h-screen bg-white relative">
+  const communities = ["NLNC", "NUXC", "NCPC", "NADC", "NWDC", "NDDC"];
+  return (
+    <div className="h-screen bg-white relative flex flex-col items-center justify-center gap-5">
       <CommunitiesLines />
-      <h1 className="text-6xl text-[#53585F] font-bold text-center mb-[80px]">Our Communities</h1>
-      <div className="grid grid-cols-3 px-[15%]">
-        {
-          communities.map((community) => {
-            return(
-              <CommunityCard image={`/NL Assets/${community}.png`} />
-            )
-          })
-        }
+      <h1 className="text-6xl text-[#53585F] font-bold text-center z-10">
+        Our Communities
+      </h1>
+      <div className="grid grid-cols-3 mx-[22%] gap-x-4 gap-y-5 relative z-10">
+        {communities.map((community) => {
+          return <CommunityCard image={`/NL Assets/${community}.png`} />;
+        })}
+        {/* <div className="bg-dark-red h-[50%] w-full absolute bottom-0 left-0 z-1"></div> */}
       </div>
     </div>
-  )
-}
+  );
+};
--- components/Home/Communities/CommunityCard.tsx
+++ components/Home/Communities/CommunityCard.tsx
@@ -1,13 +1,19 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface PropsInterface {
-  image:string
+  image: string;
 }
 
-export const CommunityCard = (props:PropsInterface) => {
-  return(
-    <div className="w-fit aspect-square bg-white outline p-4 rounded-[20px] grid place-items-center">
-      <Image src={props.image} width={500} height={500} alt="Community Logo" className="w-[50%]"/>
+export const CommunityCard = (props: PropsInterface) => {
+  return (
+    <div className="w-fit aspect-square bg-transparent outline outline-2 p-1 rounded-[20px] grid place-items-center cursor-pointer z-10 hover:shadow-[0_0_10px_1px_#000000] bg-slate-600">
+      <Image
+        src={props.image}
+        width={500}
+        height={500}
+        alt="Community Logo"
+        className="w-3/4"
+      />
     </div>
-  )
-}
+  );
+};
