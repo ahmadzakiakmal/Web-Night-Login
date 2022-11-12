eca4845570352597421b24376918094e6c2c065f
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-12 12:03:04 +0700

    feat: done Footer

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
-import { FaFacebook, FaTwitter, FaLinkedin, FaYoutube, FaInstagram, FaGooglePlus, FaPinterest, FaRss } from "react-icons/fa"
+import {
+  FaFacebook,
+  FaTwitter,
+  FaLinkedin,
+  FaYoutube,
+  FaInstagram,
+  FaGooglePlus,
+  FaPinterest,
+  FaRss,
+} from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { AiFillPrinter } from "react-icons/ai";
 
 export const Footer = () => {
   return (
@@ -15,35 +27,35 @@ export const Footer = () => {
           />
         </div>
         <div className="text-white h-[150px] flex flex-col w-fit justify-between m-4 gap-3">
-          <div>Depok, Sleman, Yogyakarta</div>
+          <div className="flex items-center gap-2" ><IoLocationSharp/> Depok, Sleman, Yogyakarta</div>
           <div className="flex justify-between">
-            <div>(123) 456-7890</div>
-            <div>(123) 456-7890</div>
+            <div className="flex items-center gap-2"><BsFillTelephoneFill/> (123) 456-7890</div>
+            <div className="flex items-center gap-2"><AiFillPrinter /> (123) 456-7890</div>
           </div>
           <div className="flex gap-5 mt-8">
             <h1 className="mr-10">Social Media</h1>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaFacebook />
             </a>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaTwitter />
             </a>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaLinkedin />
             </a>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaYoutube />
             </a>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaInstagram />
             </a>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaGooglePlus />
             </a>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaPinterest />
             </a>
-            <a href="#" className="button hover:text-blue-200">
+            <a href="#!" className="button hover:text-blue-200">
               <FaRss />
             </a>
           </div>
@@ -51,13 +63,11 @@ export const Footer = () => {
       </div>
       <div className="text-white flex justify-between">
         <div className="flex w-[40%] justify-between">
-          <h1>ABOUT</h1>
-          <h1>EVENTS</h1>
-          <h1>COMMUNITIES</h1>
+          <a href="#About" className="hover:text-blue-200">ABOUT</a>
+          <a href="#Events" className="hover:text-blue-200">EVENTS</a>
+          <a href="#Communities" className="hover:text-blue-200">COMMUNITIES</a>
         </div>
-        <div>
-          Copyright &copy; 2022 | Night Login.
-        </div>
+        <div>Copyright &copy; 2022 | Night Login.</div>
       </div>
       <div className="line bg-white bg-opacity-70 my-5"></div>
     </div>
