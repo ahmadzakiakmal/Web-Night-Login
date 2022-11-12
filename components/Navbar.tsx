dd90bc1ea2dbaf599e899e29a743302d65b5588f
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-12 12:22:34 +0700

    fix: add links to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export const Navbar = () => {
   return (
     <div className="bg-black h-[8vh] flex justify-between w-full px-5">
       {/* Logo */}
       <div className="h-full flex items-center gap-3">
-        <div className="h-[70%] aspect-square">
+        <Link href="/" className="h-[70%] aspect-square">
           <Image
             src="/NL Assets/Night Login/square.png"
             width={2000}
@@ -13,7 +15,7 @@ export const Navbar = () => {
             alt="Night Login Logo"
             priority={true}
           />
-        </div>
+        </Link>
         <div className="font-bold font-Poppins text-dark-red">
           <h1>NIGHT</h1>
           <h1>LOGIN</h1>
@@ -21,14 +23,22 @@ export const Navbar = () => {
       </div>
       {/* Nav Links */}
       <div className="flex gap-12 text-white items-center">
-        <p>About</p>
-        <p>About</p>
-        <p>About</p>
+        <a href="#About" className="hover:text-red-600">About</a>
+        <a href="#Events" className="hover:text-red-600">Events</a>
+        <a href="#Communities" className="hover:text-red-600">Communities</a>
       </div>
       {/* Search */}
       <div className="flex items-center">
-        <form onSubmit={(e) => {e.preventDefault} }>
-          <input className="rounded px-1" type="text" placeholder="🔍 Search" />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault;
+          }}
+          className="flex items-center gap-2"
+        >
+          <input className="rounded px-1" type="text" placeholder="Search"/>
+          <div className="text-white button hover:text-red-600 p-3">
+            <AiOutlineSearch />
+          </div>
         </form>
       </div>
     </div>
