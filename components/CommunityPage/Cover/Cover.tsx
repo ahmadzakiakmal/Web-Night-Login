1002c56fc5a4a4cdd6514cb12ff411e499992518
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 11:05:55 +0700

    fix: minor adjustments

--- components/CommunityPage/Cover/Cover.tsx
+++ components/CommunityPage/Cover/Cover.tsx
@@ -11,24 +11,24 @@ export const Cover = (props: PropsInterface) => {
   return (
     <div className="h-[92vh] relative flex items-center">
       <CoverImage shortName={props.shortName} />
-      <div className="flex bg-red-100 w-fit mx-auto justify-between p-[40px] gap-32">
+      <div className="flex mt-8 w-fit mx-auto justify-between p-[40px] gap-32">
         {/* Logo */}
         <div className="z-10 grid place-items-center">
           <Image
             src={`/NL Assets/${props.shortName}.png`}
-            width={300}
-            height={300}
+            width={400}
+            height={400}
             alt="community logo"
             priority={true}
           />
         </div>
         {/* About */}
         <div className="z-10 flex flex-col justify-center">
-          <div className="mb-10">
+          <div className="mb-5">
             <h1 className="text-[90px] font-bold text-[#494949] tracking-widest">
               {props.shortName}
             </h1>
-            <h2 className="mt-[-30px]  text-[#4e4e4e]">{`NIGHTLOGIN ${props.communityName.toLocaleUpperCase()} COMMUNITY`}</h2>
+            <h2 className="mt-[-30px]  text-[#4e4e4e] max-w-[40ch]">{`NIGHTLOGIN ${props.communityName.toLocaleUpperCase()} COMMUNITY`}</h2>
           </div>
           <p className="max-w-[30ch] text-[22px]">{props.about}</p>
         </div>
--- components/CommunityPage/Cover/CoverImage.tsx
+++ components/CommunityPage/Cover/CoverImage.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 
 interface PropsInterface {
-  shortName : string;
+  shortName: string;
 }
 
-export const CoverImage = (props:PropsInterface) => {
-  return(
+export const CoverImage = (props: PropsInterface) => {
+  return (
     <div className="h-[70%] aspect-square absolute top-0 left-0 z-0">
-      <Image src={`/NL Assets/${props.shortName}-cover.png`} width={1000} height={1000} alt="cover image"/>
+      <Image
+        src={`/NL Assets/${props.shortName}-cover.png`}
+        width={1000}
+        height={1000}
+        alt="cover image"
+        priority={true}
+      />
     </div>
-  )
-}
+  );
+};
