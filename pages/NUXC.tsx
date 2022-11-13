975fea93c5e7325e09cc9e4138a02a423538e613
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 11:07:07 +0700

    feat: add NUXC cover

--- pages/NUXC.tsx
+++ pages/NUXC.tsx
@@ -1,5 +1,18 @@
+import { Cover } from "../components/CommunityPage/Cover/Cover";
+import { PageHead } from "../components/CommunityPage/PageHead";
+import { Navbar } from "../components/Navbar";
+
 export default function NUXC() {
-  return(
-    <h1>NUXC</h1>
-  )
+  const about = "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design."
+  return (
+    <div>
+      <PageHead communityName="User Experience" shortName="NUXC" />
+      <main>
+        {/* Navbar */}
+        <Navbar />
+        {/* Cover */}
+        <Cover communityName="User Experience" shortName="NUXC" about={about}/>
+      </main>
+    </div>
+  );
 }
