55de8dd17c8cd33c0f04e152e2746fc19d154612
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 11:07:00 +0700

    feat: add NDDC cover

--- pages/NDDC.tsx
+++ pages/NDDC.tsx
@@ -1,5 +1,18 @@
+import { Cover } from "../components/CommunityPage/Cover/Cover";
+import { PageHead } from "../components/CommunityPage/PageHead";
+import { Navbar } from "../components/Navbar";
+
 export default function NDDC() {
-  return(
-    <h1>NDDC</h1>
-  )
+  const about = "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design."
+  return (
+    <div>
+      <PageHead communityName="Data Science and Data Analytics" shortName="NDDC" />
+      <main>
+        {/* Navbar */}
+        <Navbar />
+        {/* Cover */}
+        <Cover communityName="Data Science and Data Analytics" shortName="NDDC" about={about}/>
+      </main>
+    </div>
+  );
 }
