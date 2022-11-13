3a82a124108a1f49cbf9135f60096620f8804c88
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 10:56:56 +0700

    feat: add NWDC cover

--- pages/NWDC.tsx
+++ pages/NWDC.tsx
@@ -1,5 +1,18 @@
+import { Cover } from "../components/CommunityPage/Cover/Cover";
+import { PageHead } from "../components/CommunityPage/PageHead";
+import { Navbar } from "../components/Navbar";
+
 export default function NWDC() {
-  return(
-    <h1>NWDC</h1>
-  )
+  const about = "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design."
+  return (
+    <div>
+      <PageHead communityName="Web Development" shortName="NWDC" />
+      <main>
+        {/* Navbar */}
+        <Navbar />
+        {/* Cover */}
+        <Cover shortName="NWDC" communityName="Web Development" about={about}/>
+      </main>
+    </div>
+  );
 }
