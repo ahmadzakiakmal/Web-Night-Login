5e45b7e71f40026538f939a20a7619bee8c696fb
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 15:34:52 +0700

    feat: add placeholder photo

--- components/Home/OurTeam/Card.tsx
+++ components/Home/OurTeam/Card.tsx
@@ -1,3 +1,5 @@
+import { FaUserAlt } from "react-icons/fa";
+
 interface PropsInterface {
   name: string;
   position: string;
@@ -6,7 +8,10 @@ interface PropsInterface {
 export const Card = (props:PropsInterface) => {
   return(
     <div className="bg-white min-h-[200px] w-[250px] rounded-2xl shadow-[0px_0px_15px] relative overflow-hidden shadow-[rgba(255,255,255,0.4)]">
-      <div className="h-[30%] w-full absolute bottom-0 left-0 z-10 flex flex-col items-center justify-center bg-dark-red p-2">
+      <div className="text-[130px] grid place-items-center w-full h-full">
+        <FaUserAlt />
+      </div>
+      <div className="min-h-[30%] w-full absolute bottom-0 left-0 z-10 flex flex-col items-center justify-center bg-dark-red p-2">
         <h1 className="text-xl text-white font-bold">{props.name}</h1>
         <h1 className="text-md text-white">{props.position}</h1>
       </div>
