41674418803c21d20fa8904677a0858a31c50ed4
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 10:56:18 +0700

    feat: combined Cover into 1 comp

---
+++ components/CommunityPage/Cover/Cover.tsx
@@ -0,0 +1,38 @@
+import { CoverImage } from "./CoverImage";
+import Image from "next/image";
+
+interface PropsInterface {
+  shortName: string;
+  communityName: string;
+  about: string;
+}
+
+export const Cover = (props: PropsInterface) => {
+  return (
+    <div className="h-[92vh] relative flex items-center">
+      <CoverImage shortName={props.shortName} />
+      <div className="flex bg-red-100 w-fit mx-auto justify-between p-[40px] gap-32">
+        {/* Logo */}
+        <div className="z-10 grid place-items-center">
+          <Image
+            src={`/NL Assets/${props.shortName}.png`}
+            width={300}
+            height={300}
+            alt="community logo"
+            priority={true}
+          />
+        </div>
+        {/* About */}
+        <div className="z-10 flex flex-col justify-center">
+          <div className="mb-10">
+            <h1 className="text-[90px] font-bold text-[#494949] tracking-widest">
+              {props.shortName}
+            </h1>
+            <h2 className="mt-[-30px]  text-[#4e4e4e]">{`NIGHTLOGIN ${props.communityName.toLocaleUpperCase()} COMMUNITY`}</h2>
+          </div>
+          <p className="max-w-[30ch] text-[22px]">{props.about}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/CommunityPage/Cover/CoverImage.tsx
+++ components/CommunityPage/Cover/CoverImage.tsx
@@ -4,9 +4,9 @@ interface PropsInterface {
   shortName : string;
 }
 
-export const Cover = (props:PropsInterface) => {
+export const CoverImage = (props:PropsInterface) => {
   return(
-    <div className="h-[70%] aspect-square">
+    <div className="h-[70%] aspect-square absolute top-0 left-0 z-0">
       <Image src={`/NL Assets/${props.shortName}-cover.png`} width={1000} height={1000} alt="cover image"/>
     </div>
   )
