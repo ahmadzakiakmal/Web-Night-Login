c85eb4e0dccabaea21129515a69528c99f806cd2
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 11:06:52 +0700

    feat: add NCPC cover

--- pages/NCPC.tsx
+++ pages/NCPC.tsx
@@ -1,5 +1,18 @@
+import { Cover } from "../components/CommunityPage/Cover/Cover";
+import { PageHead } from "../components/CommunityPage/PageHead";
+import { Navbar } from "../components/Navbar";
+
 export default function NCPC() {
-  return(
-    <h1>NCPC</h1>
-  )
+  const about = "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design."
+  return (
+    <div>
+      <PageHead communityName="Competitive Programming" shortName="NCPC" />
+      <main>
+        {/* Navbar */}
+        <Navbar />
+        {/* Cover */}
+        <Cover communityName="Competitive Programming" shortName="NCPC" about={about}/>
+      </main>
+    </div>
+  );
 }
