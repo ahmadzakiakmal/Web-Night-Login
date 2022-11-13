645c357ae9676225f1ada44f6dba383a73d8aa99
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-13 11:06:43 +0700

    feat: add NADC cover

--- pages/NADC.tsx
+++ pages/NADC.tsx
@@ -1,5 +1,18 @@
+import { Cover } from "../components/CommunityPage/Cover/Cover";
+import { PageHead } from "../components/CommunityPage/PageHead";
+import { Navbar } from "../components/Navbar";
+
 export default function NADC() {
-  return(
-    <h1>NADC</h1>
-  )
+  const about = "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design."
+  return (
+    <div>
+      <PageHead communityName="Application Development" shortName="NADC" />
+      <main>
+        {/* Navbar */}
+        <Navbar />
+        {/* Cover */}
+        <Cover communityName="Application Development" shortName="NADC" about={about}/>
+      </main>
+    </div>
+  );
 }
