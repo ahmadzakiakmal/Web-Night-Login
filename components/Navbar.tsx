06097132675b0733e2e0a97f49f62b5d2b95a678
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-14 18:56:53 +0700

    fix: text not included in Link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <div className="bg-black h-[8vh] flex justify-between w-full px-5">
       {/* Logo */}
       <div className="h-full flex items-center gap-3">
-        <Link href="/" className="h-[70%] aspect-square">
+        <Link href="/" className="h-[70%] aspect-square flex items-center gap-3">
           <Image
             src="/NL Assets/Night Login/square.png"
             width={2000}
@@ -15,17 +15,23 @@ export const Navbar = () => {
             alt="Night Login Logo"
             priority={true}
           />
+          <div className="font-bold font-Poppins text-dark-red">
+            <h1>NIGHT</h1>
+            <h1>LOGIN</h1>
+          </div>
         </Link>
-        <div className="font-bold font-Poppins text-dark-red">
-          <h1>NIGHT</h1>
-          <h1>LOGIN</h1>
-        </div>
       </div>
       {/* Nav Links */}
       <div className="flex gap-12 text-white items-center">
-        <a href="#About" className="hover:text-red-600">About</a>
-        <a href="#Events" className="hover:text-red-600">Events</a>
-        <a href="#Communities" className="hover:text-red-600">Communities</a>
+        <a href="#About" className="hover:text-red-600">
+          About
+        </a>
+        <a href="#Events" className="hover:text-red-600">
+          Events
+        </a>
+        <a href="#Communities" className="hover:text-red-600">
+          Communities
+        </a>
       </div>
       {/* Search */}
       <div className="flex items-center">
@@ -35,7 +41,7 @@ export const Navbar = () => {
           }}
           className="flex items-center gap-2"
         >
-          <input className="rounded px-1" type="text" placeholder="Search"/>
+          <input className="rounded px-1" type="text" placeholder="Search" />
           <div className="text-white button hover:text-red-600 p-3">
             <AiOutlineSearch />
           </div>
