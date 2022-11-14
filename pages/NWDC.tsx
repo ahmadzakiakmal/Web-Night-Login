f9c3f1a1b1acb41068e4c26adfcaf6b2bad06b08
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-14 20:59:37 +0700

    feat: adjustments

--- pages/NWDC.tsx
+++ pages/NWDC.tsx
@@ -1,9 +1,11 @@
 import { Cover } from "../components/CommunityPage/Cover/Cover";
 import { PageHead } from "../components/CommunityPage/PageHead";
+import { EventsLines } from "../components/Home/Events.tsx/EventsLines";
 import { Navbar } from "../components/Navbar";
 
 export default function NWDC() {
-  const about = "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design."
+  const about =
+    "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design.";
   return (
     <div>
       <PageHead communityName="Web Development" shortName="NWDC" />
@@ -11,7 +13,14 @@ export default function NWDC() {
         {/* Navbar */}
         <Navbar />
         {/* Cover */}
-        <Cover shortName="NWDC" communityName="Web Development" about={about}/>
+        <Cover shortName="NWDC" communityName="Web Development" about={about} accentColor="bg-[#8CCBED]" />
+        {/* Content */}
+        <div className="min-h-screen w-full bg-[#3D81A5] relative">
+          <EventsLines />
+          <div>
+            <h1>Projects Highlights</h1>
+          </div>
+        </div>
       </main>
     </div>
   );
