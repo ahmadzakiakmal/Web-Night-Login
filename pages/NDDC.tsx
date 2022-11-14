741f93aeea5709143a0f0bdff984f02fe6b2a40a
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-14 21:14:10 +0700

    feat: done Cover

--- pages/NDDC.tsx
+++ pages/NDDC.tsx
@@ -11,7 +11,7 @@ export default function NDDC() {
         {/* Navbar */}
         <Navbar />
         {/* Cover */}
-        <Cover communityName="Data Science and Data Analytics" shortName="NDDC" about={about}/>
+        <Cover communityName="Data Science and Data Analytics" shortName="NDDC" about={about} accentColor="bg-[#858485]" />
       </main>
     </div>
   );
