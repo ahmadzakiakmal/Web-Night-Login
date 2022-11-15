fbd1ad60e7f20127b3e75145f49a32380e3188d1
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-15 18:40:01 +0700

    feat(Home/Communities): add AOS

--- components/Home/Communities/Communities.tsx
+++ components/Home/Communities/Communities.tsx
@@ -12,7 +12,28 @@ export const Communities = () => {
       </h1>
       <div className="grid grid-cols-3 mx-[22%] gap-x-4 gap-y-5 relative z-10">
         {communities.map((community, index) => {
-          return <CommunityCard name={community} key={index} />;
+          let delay= "0";
+          switch (index) {
+            case 0:
+              delay = "0";
+              break;
+            case 1:
+              delay = "300";
+              break;
+            case 2:
+              delay = "600";
+              break;
+            case 3:
+              delay = "900";
+              break;
+            case 4:
+              delay = "1200";
+              break;
+            case 5:
+              delay = "1500";
+              break;
+          }
+          return <CommunityCard name={community} key={index} delay={delay} />;
         })}
         {/* <div className="bg-dark-red h-[50%] w-full absolute bottom-0 left-0 z-1"></div> */}
       </div>
--- components/Home/Communities/CommunityCard.tsx
+++ components/Home/Communities/CommunityCard.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 interface PropsInterface {
   name: string;
+  delay: string;
 }
 
 export const CommunityCard = (props: PropsInterface) => {
   return (
-    <Link href={`/${props.name}`} className="w-fit aspect-square bg-transparent outline outline-2 p-1 rounded-[20px] grid place-items-center cursor-pointer z-10 hover:shadow-[0_0_10px_1px_#000000] bg-slate-600 hover:bg-opacity-95 bg-opacity-100">
+    <Link href={`/${props.name}`} className="w-fit aspect-square bg-transparent outline outline-2 p-1 rounded-[20px] grid place-items-center cursor-pointer z-10 hover:shadow-[0_0_10px_1px_#000000] bg-slate-600 hover:bg-opacity-95 bg-opacity-100" data-aos="fade-up"
+    data-aos-delay={props.delay}>
       <Image
         src={`/NL Assets/${props.name}.png`}
         width={500}
