9ec13c0678d8460b8da8a9945a45cfbace4b8e76
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-15 19:55:13 +0700

    feat(NCPC): add AOS

--- pages/NCPC.tsx
+++ pages/NCPC.tsx
@@ -28,24 +28,24 @@ export default function NCPC() {
           <div className="py-32">
             {/* Projects Showcase */}
             <div className="min-h-[500px] py-10">
-              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]">
+              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]" data-aos="fade-right" >
                 Projects Showcase
               </h1>
-              <div className="grid grid-cols-3 pt-16 pb-8 place-items-center px-[5%] gap-3">
+              <div className="grid grid-cols-3 pt-16 pb-8 place-items-center px-[5%] gap-3" data-aos="fade-up">
                 <EventCard title="Project 1" desc="" image="" />
                 <EventCard title="Project 2" desc="" image="" />
                 <EventCard title="Project 3" desc="" image="" />
               </div>
-              <div className="button bg-white text-[#38C063] w-fit mx-auto text-[25px] px-[15px] py-[5px] rounded font-bold font-Lato">
+              <div className="button bg-white text-[#38C063] w-fit mx-auto text-[25px] px-[15px] py-[5px] rounded font-bold font-Lato" data-aos="fade-right">
                 See More
               </div>
             </div>
             {/* Events */}
             <div className="min-h-[500px] pt-16 pb-8">
-              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]">
+              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]" data-aos="fade-right">
                 Community Events
               </h1>
-              <div className="grid grid-cols-3 py-12 place-items-center px-[5%] gap-3">
+              <div className="grid grid-cols-3 py-12 place-items-center px-[5%] gap-3" data-aos="fade-up">
                 <EventCard title="NL Web Competition" desc="" image="" />
                 <EventCard title="Internal Workshop" desc="" image="" />
                 <EventCard title="Projects Competition" desc="" image="" />
