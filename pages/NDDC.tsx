fae78331bead127d7619709deee5682b61e3fe2e
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-15 10:59:45 +0700

    feat: done layout NDDC

--- pages/NDDC.tsx
+++ pages/NDDC.tsx
@@ -1,17 +1,59 @@
 import { Cover } from "../components/CommunityPage/Cover/Cover";
 import { PageHead } from "../components/CommunityPage/PageHead";
 import { Navbar } from "../components/Navbar";
+import { EventCard } from "../components/Home/Events.tsx/EventCard";
+import { EventsLines } from "../components/Home/Events.tsx/EventsLines";
 
 export default function NDDC() {
-  const about = "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design."
+  const about =
+    "Merupakan komunitas bagi mahasiswa yang memiliki ketertarikan pada web baik itu web development maupun web design.";
   return (
     <div>
-      <PageHead communityName="Data Science and Data Analytics" shortName="NDDC" />
+      <PageHead
+        communityName="Data Science and Data Analytics"
+        shortName="NDDC"
+      />
       <main>
         {/* Navbar */}
         <Navbar />
         {/* Cover */}
-        <Cover communityName="Data Science and Data Analytics" shortName="NDDC" about={about} accentColor="bg-[#858485]" />
+        <Cover
+          communityName="Data Science and Data Analytics"
+          shortName="NDDC"
+          about={about}
+          accentColor="bg-[#858485]"
+        />
+        {/* Content */}
+        <div className="min-h-screen w-full bg-[#312F2F] relative">
+          <EventsLines />
+          <div className="py-32">
+            {/* Projects Showcase */}
+            <div className="min-h-[500px] py-10">
+              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]">
+                Projects Showcase
+              </h1>
+              <div className="grid grid-cols-3 pt-16 pb-8 place-items-center px-[5%] gap-3">
+                <EventCard title="Project 1" desc="" image="" />
+                <EventCard title="Project 2" desc="" image="" />
+                <EventCard title="Project 3" desc="" image="" />
+              </div>
+              <div className="button bg-white text-[#252525] w-fit mx-auto text-[25px] px-[15px] py-[5px] rounded font-bold font-Lato">
+                See More
+              </div>
+            </div>
+            {/* Events */}
+            <div className="min-h-[500px] pt-16 pb-8">
+              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]">
+                Community Events
+              </h1>
+              <div className="grid grid-cols-3 py-12 place-items-center px-[5%] gap-3">
+                <EventCard title="NL Web Competition" desc="" image="" />
+                <EventCard title="Internal Workshop" desc="" image="" />
+                <EventCard title="Projects Competition" desc="" image="" />
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
