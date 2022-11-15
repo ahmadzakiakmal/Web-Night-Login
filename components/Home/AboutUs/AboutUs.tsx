26486b39a892088002a0ab8c2a47b4e0c95f33f5
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-15 18:15:53 +0700

    feat(Home/AboutUs): add AOS

--- components/Home/AboutUs/AboutUs.tsx
+++ components/Home/AboutUs/AboutUs.tsx
@@ -3,7 +3,10 @@ import { AboutUsLines } from "./AboutUsLines";
 
 export const AboutUs = () => {
   return (
-    <div id="About" className="w-full bg-white relative h-[100vh] flex justify-center items-center">
+    <div
+      id="About"
+      className="w-full bg-white relative h-[100vh] flex justify-center items-center"
+    >
       {/* png / svg  */}
       <div className="w-[20%] h-[100vh] bg-dark-red absolute top-0 right-0 z-0"></div>
       <AboutUsLines />
@@ -11,16 +14,21 @@ export const AboutUs = () => {
       <div className="z-10 w-[90%]">
         <div className="flex text-[24px] justify-center">
           <div className="max-w-[50ch] pr-5 self-center">
-            <h1 className="text-6xl text-[#494949] font-bold mb-[20px] w-fit">
+            <h1
+              className="text-6xl text-[#494949] font-bold mb-[20px] w-fit"
+              data-aos="fade-right"
+            >
               About Us
             </h1>
-            Night Login merupakan Badan Semi Otonom (BSO) di DTETI FT UGM yang
-            bergerak di bidang teknologi informasi. BSO ini merupakan wadah bagi
-            mahasiswa DTETI yang memiliki minat di bidang IT untuk mengembangkan
-            kemampuan, kreativitas, inovasi, dan memperluas wawasan terkait
-            bidang-bidang IT.
+            <p data-aos="fade-right" data-aos-delay="500">
+              Night Login merupakan Badan Semi Otonom (BSO) di DTETI FT UGM yang
+              bergerak di bidang teknologi informasi. BSO ini merupakan wadah
+              bagi mahasiswa DTETI yang memiliki minat di bidang IT untuk
+              mengembangkan kemampuan, kreativitas, inovasi, dan memperluas
+              wawasan terkait bidang-bidang IT.
+            </p>
           </div>
-          <div className="h-[300px] aspect-square">
+          <div className="h-[300px] aspect-square" data-aos="fade-left" >
             <Image
               src="/NL Assets/Night Login/square.png"
               width={2000}
