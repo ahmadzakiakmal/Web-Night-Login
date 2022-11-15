64cc6d64aaf67b89afe37dff18c35648b308d6db
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-15 19:54:42 +0700

    feat(Community/Cover): add AOS

--- components/CommunityPage/Cover/Cover.tsx
+++ components/CommunityPage/Cover/Cover.tsx
@@ -17,7 +17,7 @@ export const Cover = (props: PropsInterface) => {
       <CoverImage shortName={props.shortName} />
       <div className="flex mt-8 mx-auto justify-between w-[70%] gap-16">
         {/* Logo */}
-        <div className="z-10 grid place-items-center">
+        <div className="z-10 grid place-items-center" data-aos="fade-right">
           <Image
             src={`/NL Assets/${props.shortName}.png`}
             width={400}
@@ -29,17 +29,40 @@ export const Cover = (props: PropsInterface) => {
         {/* About */}
         <div className="z-10 flex flex-col justify-center">
           <div className="mb-5">
-            <h1 className="text-[90px] mt-[-30px] font-bold text-[#494949] tracking-widest">
+            <h1
+              className="text-[90px] mt-[-30px] font-bold text-[#494949] tracking-widest"
+              data-aos="fade-left"
+            >
               {props.shortName}
             </h1>
-            <h2 className="mt-[-30px]  text-[#4e4e4e] max-w-[40ch]">{`NIGHTLOGIN ${props.communityName.toLocaleUpperCase()} COMMUNITY`}</h2>
+            <h2
+              className="mt-[-30px]  text-[#4e4e4e] max-w-[40ch]"
+              data-aos="fade-left"
+              data-aos-delay="300"
+            >{`NIGHTLOGIN ${props.communityName.toLocaleUpperCase()} COMMUNITY`}</h2>
           </div>
-          <p className="max-w-[35ch] text-[22px] text-[#4e4e4e] mb-10">
+          <p
+            className="max-w-[35ch] text-[22px] text-[#4e4e4e] mb-10"
+            data-aos="fade-left"
+            data-aos-delay="600"
+          >
             {props.about}
           </p>
-          <div className="flex justify-between gap-3">
-            <CommunityTeam name={props.headName} position="Head" bgColor={props.accentColor}/>
-            <CommunityTeam name={props.viceName} position="Vice" bgColor={props.accentColor}/>
+          <div
+            className="flex justify-between gap-3"
+            data-aos="fade-left"
+            data-aos-delay="900"
+          >
+            <CommunityTeam
+              name={props.headName}
+              position="Head"
+              bgColor={props.accentColor}
+            />
+            <CommunityTeam
+              name={props.viceName}
+              position="Vice"
+              bgColor={props.accentColor}
+            />
           </div>
         </div>
       </div>
