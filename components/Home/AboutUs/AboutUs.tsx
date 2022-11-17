61a20603f194a3b2c3c694948ce8a69100f44d45
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-17 14:56:09 +0700

    fix(Home/AboutUs): responsive adjustments

--- components/Home/AboutUs/AboutUs.tsx
+++ components/Home/AboutUs/AboutUs.tsx
@@ -5,7 +5,7 @@ export const AboutUs = () => {
   return (
     <div
       id="About"
-      className="w-full bg-white relative min-h-[100vh] flex justify-center items-center"
+      className="w-full bg-white relative py-[20%] lg:py-0 lg:min-h-[100vh] flex justify-center items-center"
     >
       {/* png / svg  */}
       <div className="md:w-[20%] w-full h-full bg-dark-red absolute top-0 right-0 z-0"></div>
@@ -13,14 +13,14 @@ export const AboutUs = () => {
       {/* content */}
       <div className="z-10 w-[90%] lg:w-[75%]">
         <div className="flex justify-center items-center flex-col-reverse md:flex-row lg:justify-between">
-          <div className="max-w-[60ch] md:text-[#494949] text-white pr-5 self-center">
+          <div className="max-w-[60ch] md:text-[#494949] text-white md:pr-5 self-center">
             <h1
               className="text-5xl md:text-6xl font-bold mb-[20px] text-center md:text-left"
               data-aos="fade-right"
             >
               About Us
             </h1>
-            <p className="md:text-[24px] text-[20px] text-justify md:text-left" data-aos="fade-right" data-aos-delay="500">
+            <p className="text-[20px] md:text-[22px] lg:text-[24px] text-justify md:text-left" data-aos="fade-right" data-aos-delay="500">
               Night Login merupakan Badan Semi Otonom (BSO) di DTETI FT UGM yang
               bergerak di bidang teknologi informasi. BSO ini merupakan wadah
               bagi mahasiswa DTETI yang memiliki minat di bidang IT untuk
@@ -28,7 +28,7 @@ export const AboutUs = () => {
               wawasan terkait bidang-bidang IT.
             </p>
           </div>
-          <div className="w-[60%] md:w-[200px] lg:w-[300px] aspect-square mb-10 md:mb-0 md:self-center" data-aos="fade-left" >
+          <div className="w-[60%] md:w-[200px] lg:w-[300px] flex-shrink-0 aspect-square mb-10 md:mb-0 md:self-center" data-aos="fade-left" >
             <Image
               src="/NL Assets/Night Login/square.png"
               width={2000}
