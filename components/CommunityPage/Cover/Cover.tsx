159ef9ca310737a3486ae5e7afec5f8511dcf3eb
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-18 20:45:19 +0700

    fix(CommunityPages/Cover): minor responsive changes

--- components/CommunityPage/Cover/Cover.tsx
+++ components/CommunityPage/Cover/Cover.tsx
@@ -13,7 +13,7 @@ interface PropsInterface {
 
 export const Cover = (props: PropsInterface) => {
   return (
-    <div className="min-h-[92vh] relative flex items-center bg-[#FCFFFE] overflow-x-hidden">
+    <div className="min-h-[92vh] relative flex items-center bg-[#FCFFFE] overflow-x-hidden mt-[8vh]">
       <CoverImage shortName={props.shortName} />
       <div className="flex flex-col lg:flex-row mt-8 mx-auto md:justify-between w-[80%] xl:w-[70%] gap-16 flex-shrink-0">
         {/* Logo */}
@@ -30,19 +30,19 @@ export const Cover = (props: PropsInterface) => {
         <div className="mt-[-50px] lg:mt-auto z-10 flex flex-col justify-center">
           <div className="mb-5">
             <h1
-              className="text-[90px] mt-[-30px] font-bold text-[#494949] tracking-widest"
+              className="text-[70px] ssm:text-[90px] mt-[-30px] font-bold text-[#494949] tracking-widest"
               data-aos="fade-left"
             >
               {props.shortName}
             </h1>
             <h2
-              className="md:text-left mt-[-30px] text-[#4e4e4e] max-w-[40ch]"
+              className="md:text-left mt-[-15px] ssm:mt-[-30px] text-[#4e4e4e] max-w-[40ch]"
               data-aos="fade-left"
               data-aos-delay="300"
             >{`NIGHTLOGIN ${props.communityName.toLocaleUpperCase()} COMMUNITY`}</h2>
           </div>
           <p
-            className="max-w-[35ch] text-[22px] text-[#4e4e4e] mb-10"
+            className="max-w-[35ch] text-[18px] ssm:text-[22px] text-[#4e4e4e] mb-10"
             data-aos="fade-left"
             data-aos-delay="600"
           >
