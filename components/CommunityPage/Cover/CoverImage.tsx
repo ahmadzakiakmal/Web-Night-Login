0bfa200efd6846d6b19851ad3d4839917df43ebc
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-18 14:16:01 +0700

    feat: various component responsive update

--- components/CommunityPage/Cover/CoverImage.tsx
+++ components/CommunityPage/Cover/CoverImage.tsx
@@ -6,7 +6,7 @@ interface PropsInterface {
 
 export const CoverImage = (props: PropsInterface) => {
   return (
-    <div className="h-[70%] aspect-square absolute top-0 left-0 z-0">
+    <div className="w-[70%] lg:h-[80%] lg:w-auto aspect-square absolute top-0 left-0 z-0">
       <Image
         src={`/NL Assets/${props.shortName}-cover.png`}
         width={1000}
--- components/Home/OurTeam/OurTeam.tsx
+++ components/Home/OurTeam/OurTeam.tsx
@@ -3,16 +3,16 @@ import { Card } from "./Card";
 
 export const OurTeam = () => {
   return (
-    <div className="bg-dark-red h-[110vh] relative flex flex-col justify-center">
+    <div className="bg-dark-red h-fit relative flex flex-col justify-center overflow-hidden">
       <AboutUsLines />
-      <h1 className="text-6xl text-white font-bold text-center z-10 mb-10" data-aos="fade-right">
+      <h1 className="text-5xl md:text-6xl text-white font-bold text-center z-10 mb-10" data-aos="fade-right">
         Team Structure
       </h1>
-      <div className="flex justify-center gap-10 mb-10" data-aos="fade-up" data-aos-delay="300">
+      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-10 mb-16 md:mb-10" data-aos="fade-up" data-aos-delay="300">
         <Card name="Samatha Marhaendra P" position="President"/>
         <Card name="Christian Ale Perdana" position="Vice" />
       </div>
-      <div className="flex justify-center gap-5" data-aos="fade-up" data-aos-delay="600">
+      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center mb-10 gap-5" data-aos="fade-up" data-aos-delay="600">
         <Card name="Sagara Aldavy" position="Head of Public Relation" />
         <Card name="Arifah Amelia T" position="Head of Creative Media" />
         <Card name="Gerrit Ezra Yudi K" position="Head of Administration" />
