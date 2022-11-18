f05332d158206162f6cbc91ec1470f0cce889244
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-11-18 14:12:57 +0700

    feat(NCPC): responsive update

--- pages/NCPC.tsx
+++ pages/NCPC.tsx
@@ -25,27 +25,42 @@ export default function NCPC() {
         {/* Content */}
         <div className="min-h-screen w-full bg-[#30A758] relative">
           <EventsLines />
-          <div className="py-32">
+          <div className="py-10 md:py-32">
             {/* Projects Showcase */}
             <div className="min-h-[500px] py-10">
-              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]" data-aos="fade-right" >
+              <h1
+                className="text-5xl md:text-6xl font-bold tracking-wide text-center text-[#FCFFFE]"
+                data-aos="fade-right"
+              >
                 Projects Showcase
               </h1>
-              <div className="grid grid-cols-3 pt-16 pb-8 place-items-center px-[5%] gap-3" data-aos="fade-up">
+              <div
+                className="grid grid-cols-1 md:grid-cols-3 pt-16 pb-8 place-items-center px-[10%] md:px-[5%] gap-10 md:gap-3"
+                data-aos="fade-up"
+              >
                 <EventCard title="Project 1" desc="" image="" />
                 <EventCard title="Project 2" desc="" image="" />
                 <EventCard title="Project 3" desc="" image="" />
               </div>
-              <div className="button bg-white text-[#38C063] w-fit mx-auto text-[25px] px-[15px] py-[5px] rounded font-bold font-Lato" data-aos="fade-right">
+              <div
+                className="button bg-white text-[#C5BA8E] w-fit mx-auto text-[25px] px-[15px] py-[5px] rounded font-bold font-Lato"
+                data-aos="fade-right"
+              >
                 See More
               </div>
             </div>
             {/* Events */}
             <div className="min-h-[500px] pt-16 pb-8">
-              <h1 className="text-6xl font-bold tracking-wide text-center text-[#FCFFFE]" data-aos="fade-right">
+              <h1
+                className="text-5xl md:text-6xl font-bold tracking-wide text-center text-[#FCFFFE]"
+                data-aos="fade-right"
+              >
                 Community Events
               </h1>
-              <div className="grid grid-cols-3 py-12 place-items-center px-[5%] gap-3" data-aos="fade-up">
+              <div
+                className="grid grid-cols-1 md:grid-cols-3 py-16 place-items-center px-[10%] md:px-[5%] gap-10 md:gap-3"
+                data-aos="fade-up"
+              >
                 <EventCard title="NL Web Competition" desc="" image="" />
                 <EventCard title="Internal Workshop" desc="" image="" />
                 <EventCard title="Projects Competition" desc="" image="" />
